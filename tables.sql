7baff41a9b6a4d6a6eee76030f65870264a47ed2
pcloud <pcloud@myiac.mymdc.net>
2019-10-23 14:24:56 -0400

    :D

--- tables.sql
+++ tables.sql
@@ -39,32 +39,28 @@ INSERT INTO
         product_price
     )
 VALUES
+    (009, 'iPad', 'iPad', 429),
+    (022, 'Music', 'HomePod', 299),
+    (010, 'iPad', 'iPad Air', 649),
+    (020, 'TV', 'Apple TV HD', 149),
+    (021, 'TV', 'Apple TV 4K', 199),
+    (017, 'iMac', 'iMac Pro', 4999),
     (001, 'iPhone', 'iPhone 11', 749),
     (002, 'iPhone', 'iPhone XR', 649),
+    (018, 'iMac', 'iMac 27inch', 1799),
+    (019, 'iMac', 'iMac 21.5inch', 1099),
+    (024, 'Music', 'Powerbeats Pro', 249),
     (003, 'iPhone', 'iPhone 8 Plus', 599),
     (004, 'iPhone', 'iPhone 11 Pro', 1149),
+    (011, 'iPad', 'iPad Pro 11-inch ', 949),
     (005, 'iPhone', 'iPhone 11 Pro Max', 1249),
+    (015, 'Music', 'AirPods Charging Case', 159),
     (006, 'MacBook', 'MacBook Air 13-inch', 1099),
     (007, 'MacBook', 'MacBook Pro 13-inch', 1299),
     (008, 'MacBook', 'MacBook Pro 15-inch', 2399),
-    (009, 'iPad', 'iPad', 429),
-    (010, 'iPad', 'iPad Air', 649),
-    (011, 'iPad', 'iPad Pro 11-inch ', 949),
-    (012, 'Apple Watch', 'Apple Watch Series 5', 399),
-    (013, 'Apple Watch', 'Apple Watch Hermes', 1399),
-    (014, 'Apple Watch', 'Apple Watch Edition', 799),
-    (015, 'Music', 'AirPods Charging Case', 159),
-    (
-        016,
-        'Music',
-        'AirPods w/ Wireless Charging Case',
-        199
-    ),
-    (017, 'iMac', 'iMac Pro', 4999),
-    (018, 'iMac', 'iMac 27inch', 1799),
-    (019, 'iMac', 'iMac 21.5inch', 1099),
-    (020, 'TV', 'Apple TV HD', 149),
-    (021, 'TV', 'Apple TV 4K', 199),
-    (022, 'Music', 'HomePod', 299),
     (023, 'Music', 'Beats Solo Pro Wireless', 299),
-    (024, 'Music', 'Powerbeats Pro', 249);
+    (014, 'Apple Watch', 'Apple Watch Edition', 799),
+    (013, 'Apple Watch', 'Apple Watch Hermes', 1399),
+    (012, 'Apple Watch', 'Apple Watch Series 5', 399),
+    (016, 'Music', 'AirPods w/ Wireless Charging Case', 199)
+    ;
